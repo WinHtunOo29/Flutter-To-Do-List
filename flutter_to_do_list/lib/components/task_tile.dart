149c4c3fb742c6fe90ddb522bb1ae8741cb34feb
Win Htun Oo <winhtunoo@onenex.co>
2024-10-22 20:44:25 +0700

    manage state for checkbox

--- flutter_to_do_list/lib/components/task_tile.dart
+++ flutter_to_do_list/lib/components/task_tile.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_to_do_list/components/task_checkbox.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool? isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('Task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) => {
-        
-        },
+      title: Text(
+        'Task',
+        style: TextStyle(
+          decoration: isChecked == true ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool? checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        }
+      )
     );
   }
 }
